1ef573d8034296329816dfbc0d97caa1a5346f5f
iSmart <961478120@qq.com>
2018-07-10 20:28:30 +0800

    增加现场记录页面，接待代替停止，修改图片，适配

---
+++ app/src/main/java/com/empowerment/salesrobot/ui/adapter/FieldRecordAdapter.java
@@ -0,0 +1,10 @@
+package com.empowerment.salesrobot.ui.adapter;
+
+/**
+ * Author: 小火
+ * Email:1403241630@qq.com
+ * Created by 2018/7/10.
+ * Description:
+ */
+public class FieldRecordAdapter {
+}
---
+++ app/src/main/java/com/empowerment/salesrobot/ui/model/FieldRecordBean.java
@@ -0,0 +1,10 @@
+package com.empowerment.salesrobot.ui.model;
+
+/**
+ * Author: 小火
+ * Email:1403241630@qq.com
+ * Created by 2018/7/10.
+ * Description:
+ */
+public class FieldRecordBean {
+}
