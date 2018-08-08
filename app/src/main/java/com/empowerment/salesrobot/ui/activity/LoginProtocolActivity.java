e90fcd2ab8934129ec5ca733dd225823227f4027
iSmart <961478120@qq.com>
2018-08-08 20:35:59 +0800

    个别完善

---
+++ app/src/main/java/com/empowerment/salesrobot/ui/activity/LoginProtocolActivity.java
@@ -0,0 +1,12 @@
+package com.empowerment.salesrobot.ui.activity;
+
+import com.empowerment.salesrobot.ui.base.BaseActivity;
+
+/**
+ * Author: 小火
+ * Email:1403241630@qq.com
+ * Created by 2018/8/7.
+ * Description:
+ */
+public class LoginProtocolActivity extends BaseActivity {
+}
